00774205cfb7b6cd026735d0ab7b6992bbd63afa
gen-eslam <romancawy12@gmail.com>
2023-09-12 04:19:52 +0300

    add books request model

--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -1,12 +1,26 @@
 import 'package:bookly_mvvm_bloc/core/errors/failures.dart';
+import 'package:bookly_mvvm_bloc/core/utils/api_service.dart';
 import 'package:bookly_mvvm_bloc/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_mvvm_bloc/features/home/data/models/book_model/books_request.dart';
 import 'package:bookly_mvvm_bloc/features/home/data/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
 class HomeRepoImpl implements HomeRepo {
+  final ApiService apiService;
+
+  HomeRepoImpl({required this.apiService});
+
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchNewsetBooks() async {
+    try {
+      Map<String,dynamic> request = await apiService.get(
+          endPoint:
+              "volumes?Filtering=free-ebooks&Sorting=newest &q=subject:programming")
+       return right(BooksRequest.fromJson(request).items);
+      
+    } on Exception catch (e) {
+      return left(ServerFailure());
+    }
   }
 
   @override
