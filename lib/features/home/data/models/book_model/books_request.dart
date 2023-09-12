c44a3500433b9ab03c6cdff6c2c4b2ecc2421c17
gen-eslam <romancawy12@gmail.com>
2023-09-12 04:18:17 +0300

    create a books request model

---
+++ lib/features/home/data/models/book_model/books_request.dart
@@ -0,0 +1,21 @@
+import 'package:bookly_mvvm_bloc/features/home/data/models/book_model/book_model.dart';
+import 'package:equatable/equatable.dart';
+
+class BooksRequest extends Equatable {
+  String? kind;
+  int? totalItem;
+  List<BookModel> items;
+
+  BooksRequest({required this.items, this.kind, this.totalItem});
+
+  factory BooksRequest.fromJson(Map<String, dynamic> json) => BooksRequest(
+        kind: json["kind"],
+        totalItem: json["totalItem"],
+        items: List<BookModel>.from(
+            json["items"].map((e) => BookModel.fromJson(e))),
+      );
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [kind, totalItem, items];
+}
